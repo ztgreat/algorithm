267901a214bb8f5a86032b4ca93cc0fbca3982e3
小张 <1170881778@qq.com>
2019-01-17 18:54:05 +0800

    leetcode 872

---
+++ src/main/java/com/github/ztgreat/leetcode/problem_872/Solution.java
@@ -0,0 +1,41 @@
+package com.github.ztgreat.leetcode.problem_872;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 判断两颗树 叶子节点是否一致
+ */
+class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+}
+
+class Solution {
+
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer>res1= new ArrayList<Integer>(100);
+        List<Integer>res2= new ArrayList<Integer>(100);
+        travelTree(root1,res1);
+        travelTree(root2,res2);
+        return res1.equals(res2);
+    }
+    public List<Integer> travelTree(TreeNode root,List<Integer>res) {
+        if(root==null){
+            return res;
+        }
+        if(root.left!=null){
+            travelTree(root.left,res);
+        }
+        if(root.right!=null){
+            travelTree(root.right,res);
+        }
+        if(root.left == root.right && root.left==null){
+            res.add(root.val);
+        }
+        return res;
+    }
+ }
