c1a85584b8a002a13e4286a5027802776c593e8a
小张 <1170881778@qq.com>
2019-01-22 10:25:21 +0800

    leetcode 100

---
+++ src/main/java/com/github/ztgreat/leetcode/problem_100/Solution.java
@@ -0,0 +1,33 @@
+package com.github.ztgreat.leetcode.problem_100;
+
+
+/**
+ * 判断两颗二叉树 是否相同
+ */
+class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+}
+
+class Solution {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q==null){
+            return true;
+        }
+        if((p==null && q!=null) ||(p!=null && q==null)) {
+            return false;
+        }
+        if(p.val==q.val) {
+            if(isSameTree(p.left,q.left)) {
+                return isSameTree(p.right,q.right);
+            }
+            return false;
+        }
+        else {
+            return false;
+        }
+    }
+}
