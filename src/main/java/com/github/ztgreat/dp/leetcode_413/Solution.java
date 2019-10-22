d7a7d71f4e3a3b8817e371d8ae3154408f2fdf1d
ztgreat <1170881778@qq.com>
2019-10-22 19:59:31 +0800

    等差数列划分

--- src/main/java/com/github/ztgreat/dp/leetcode_413/Solution.java
+++ src/main/java/com/github/ztgreat/dp/leetcode_413/Solution.java
@@ -8,40 +8,40 @@ class Solution {
 
     public int numberOfArithmeticSlices(int[] A) {
 
-        if(A==null || A.length<3){
+        if (A == null || A.length < 3) {
             return 0;
         }
 
-        int n=A.length;
+        int n = A.length;
         int[][] dp = new int[n][n];
 
-        for (int i=0;i<n;i++){
-            for (int j=i;j<n;j++){
-                dp[i][j]=Integer.MAX_VALUE;
-                if(i==j){
-                    dp[i][j]=0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                dp[i][j] = Integer.MAX_VALUE;
+                if (i == j) {
+                    dp[i][j] = 0;
                 }
             }
         }
-        for (int i=0;i<n;i++){
-            for (int j=i+1;j<n;j++){
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
 
-                if(i+1==j){
-                    dp[i][j]=A[j]-A[i];
+                if (i + 1 == j) {
+                    dp[i][j] = A[j] - A[i];
                     continue;
                 }
-                if(A[j]-A[j-1]==dp[i][j-1]){
-                    dp[i][j]=dp[i][j-1];
-                }else{
+                if (A[j] - A[j - 1] == dp[i][j - 1]) {
+                    dp[i][j] = dp[i][j - 1];
+                } else {
                     break;
                 }
             }
         }
-        int sum=0;
-        for (int i=0;i<n;i++){
-            for (int j=i+2;j<n;j++){
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 2; j < n; j++) {
 
-                if(dp[i][j]!=Integer.MAX_VALUE){
+                if (dp[i][j] != Integer.MAX_VALUE) {
                     sum++;
                 }
             }
