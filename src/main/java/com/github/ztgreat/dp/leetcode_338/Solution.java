db980e55ce6d40ab8319da1ac8b866e4c36f3700
ztgreat <1170881778@qq.com>
2019-10-22 19:58:54 +0800

    比特位计数

--- src/main/java/com/github/ztgreat/dp/leetcode_338/Solution.java
+++ src/main/java/com/github/ztgreat/dp/leetcode_338/Solution.java
@@ -4,23 +4,23 @@ package com.github.ztgreat.dp.leetcode_338;
 class Solution {
 
     public int[] countBits(int num) {
-        int[] dp = new int[num+1];
-        if(num==0){
-            dp[0]=0;
+        int[] dp = new int[num + 1];
+        if (num == 0) {
+            dp[0] = 0;
             return dp;
         }
-        if(num==1){
-            dp[0]=0;
-            dp[1]=1;
+        if (num == 1) {
+            dp[0] = 0;
+            dp[1] = 1;
             return dp;
         }
-        dp[0]=0;
-        dp[1]=1;
-        for (int i=2;i<=num;i++){
-            if((i&1)==0){
-                dp[i]=dp[i/2];
-            }else{
-                dp[i]=dp[i/2]+1;
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= num; i++) {
+            if ((i & 1) == 0) {
+                dp[i] = dp[i / 2];
+            } else {
+                dp[i] = dp[i / 2] + 1;
             }
         }
         return dp;
