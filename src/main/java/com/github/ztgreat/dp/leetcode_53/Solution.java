fff3c49f2e7d0bfe6aff6ca89212a1c4135ac28b
ztgreat <1170881778@qq.com>
2019-10-22 19:57:53 +0800

    最大子序和

--- src/main/java/com/github/ztgreat/dp/leetcode_53/Solution.java
+++ src/main/java/com/github/ztgreat/dp/leetcode_53/Solution.java
@@ -1,19 +1,18 @@
 package com.github.ztgreat.dp.leetcode_53;
 
 
-
 class Solution {
 
     public int maxSubArray(int[] nums) {
 
-        if(nums==null  || nums.length==0){
+        if (nums == null || nums.length == 0) {
             return 0;
         }
-        int max=nums[0];
-        for (int i=1;i<nums.length;i++){
-            nums[i]=Math.max(nums[i],nums[i]+nums[i-1]);
-            if(max<nums[i]){
-                max=nums[i];
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = Math.max(nums[i], nums[i] + nums[i - 1]);
+            if (max < nums[i]) {
+                max = nums[i];
             }
         }
         return max;
