4b67e5846c0d3d98d8c0811c0d5b52b351357f94
ztgreat <1170881778@qq.com>
2019-10-22 19:59:58 +0800

    买卖股票的最佳时机含手续费

--- src/main/java/com/github/ztgreat/dp/leetcode_714/Solution.java
+++ src/main/java/com/github/ztgreat/dp/leetcode_714/Solution.java
@@ -6,24 +6,24 @@ class Solution {
 
     public int maxProfit(int[] prices, int fee) {
 
-        if(prices==null){
+        if (prices == null) {
             return 0;
         }
-        int n=prices.length;
+        int n = prices.length;
         int[][] dp = new int[n][2];
-        for (int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
 
-            if(i==0){
-                dp[i][0]=0;
+            if (i == 0) {
+                dp[i][0] = 0;
                 //查看 dp[i][0]的递推关系
-                dp[i][1]=-fee-prices[0];
+                dp[i][1] = -fee - prices[0];
                 continue;
             }
 
-            dp[i][0]=Math.max(dp[i-1][0],dp[i-1][1]+prices[i]);
-            dp[i][1]=Math.max(dp[i-1][1],dp[i-1][0]-prices[i]-fee);
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i] - fee);
         }
-        return dp[n-1][0];
+        return dp[n - 1][0];
 
     }
 }
