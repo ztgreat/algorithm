82294e153abb52a7fa2a09128fbce9c76a22455d
ztgreat <1170881778@qq.com>
2019-10-22 20:00:51 +0800

    下降路径最小和

--- src/main/java/com/github/ztgreat/dp/leetcode_931/Solution.java
+++ src/main/java/com/github/ztgreat/dp/leetcode_931/Solution.java
@@ -5,40 +5,40 @@ class Solution {
 
     public int minFallingPathSum(int[][] A) {
 
-        if(A==null){
+        if (A == null) {
             return 0;
         }
 
-        int n= A.length;
-        int [][] dp=new int [n][n];
+        int n = A.length;
+        int[][] dp = new int[n][n];
 
-        for (int i=0;i<n;i++){
-            for (int j=0;j<n;j++){
-                if(j==0){
-                    if(i==0){
-                        dp[i][j]=A[i][j];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == 0) {
+                    if (i == 0) {
+                        dp[i][j] = A[i][j];
                         continue;
                     }
-                    dp[i][j]=Math.min(dp[i-1][j+1]+A[i][j],dp[i-1][j]+A[i][j]);
+                    dp[i][j] = Math.min(dp[i - 1][j + 1] + A[i][j], dp[i - 1][j] + A[i][j]);
                     continue;
                 }
-                if(i==0){
-                    dp[i][j]=A[i][j];
+                if (i == 0) {
+                    dp[i][j] = A[i][j];
                     continue;
                 }
-                if(j+1==n){
-                    dp[i][j]=Math.min(dp[i-1][j-1]+A[i][j],dp[i-1][j]+A[i][j]);
+                if (j + 1 == n) {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1] + A[i][j], dp[i - 1][j] + A[i][j]);
                     continue;
                 }
-                dp[i][j]=Math.min(dp[i-1][j-1]+A[i][j],dp[i-1][j+1]+A[i][j]);
-                dp[i][j]=Math.min(dp[i][j],dp[i-1][j]+A[i][j]);
+                dp[i][j] = Math.min(dp[i - 1][j - 1] + A[i][j], dp[i - 1][j + 1] + A[i][j]);
+                dp[i][j] = Math.min(dp[i][j], dp[i - 1][j] + A[i][j]);
             }
         }
 
-        int res=Integer.MAX_VALUE;
+        int res = Integer.MAX_VALUE;
 
-        for (int i=0;i<n;i++){
-            res=Math.min(dp[n-1][i],res);
+        for (int i = 0; i < n; i++) {
+            res = Math.min(dp[n - 1][i], res);
         }
         return res;
 
