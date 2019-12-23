2c16ada52c2d14e7a60f0f0464a7d4d03f4b8882
ztgreat <1170881778@qq.com>
2019-12-23 22:28:42 +0800

    克隆图

---
+++ src/main/java/com/github/ztgreat/dfs/leetcode_133/leetcode_98/Solution.java
@@ -0,0 +1,23 @@
+package com.github.ztgreat.dfs.leetcode_133.leetcode_98;
+
+
+import java.util.List;
+
+class Node {
+    public int val;
+    public List<Node> neighbors;
+
+    public Node() {
+    }
+
+    public Node(int _val, List<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+class Solution {
+    public Node cloneGraph(Node node) {
+
+    }
+}
