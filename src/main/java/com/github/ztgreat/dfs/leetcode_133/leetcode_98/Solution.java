c812199549ca0fd186c4f5bdac2af14350766b2d
ztgreat <1170881778@qq.com>
2019-12-24 21:52:43 +0800

    克隆图-dfs

--- src/main/java/com/github/ztgreat/dfs/leetcode_133/leetcode_98/Solution.java
+++ src/main/java/com/github/ztgreat/dfs/leetcode_133/leetcode_98/Solution.java
@@ -1,6 +1,8 @@
 package com.github.ztgreat.dfs.leetcode_133.leetcode_98;
 
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 class Node {
@@ -14,10 +16,44 @@ class Node {
         val = _val;
         neighbors = _neighbors;
     }
-};
+}
 
 class Solution {
-    public Node cloneGraph(Node node) {
 
+    private HashMap<Integer, Node> map;
+
+    public Node cloneGraph(Node node) {
+        if (node == null) {
+            return null;
+        }
+        Node root = new Node();
+        root.neighbors = new ArrayList<>();
+        map = new HashMap<>(128);
+        dfs(root, node);
+        return root;
+    }
+
+    public void dfs(Node root, Node target) {
+
+        if (target == null) {
+            return;
+        }
+        map.put(target.val, root);
+        root.val = target.val;
+
+        if (target.neighbors == null || target.neighbors.isEmpty()) {
+            return;
+        }
+        for (Node node : target.neighbors) {
+
+            if (map.get(node.val) != null) {
+                root.neighbors.add(map.get(node.val));
+                continue;
+            }
+            Node temp = new Node();
+            temp.neighbors = new ArrayList<>();
+            root.neighbors.add(temp);
+            dfs(temp, node);
+        }
     }
 }
