11bce47d47e287eedc758d293bad6662b5851e84
ztgreat <1170881778@qq.com>
2020-01-14 23:07:24 +0800

    二叉树的右视图

--- src/main/java/com/github/ztgreat/dfs/leetcode_199/Solution.java
+++ src/main/java/com/github/ztgreat/dfs/leetcode_199/Solution.java
@@ -1,6 +1,7 @@
 package com.github.ztgreat.dfs.leetcode_199;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 class TreeNode {
@@ -13,9 +14,31 @@ class TreeNode {
     }
 }
 
+/**
+ * 优先遍历右子树
+ * 遍历的时候记录深度信息，如果是最新的深度则需要记录结果
+ */
 class Solution {
 
+    private Integer maxRightDepth;
     public List<Integer> rightSideView(TreeNode root) {
 
+        List<Integer> ans = new ArrayList<>();
+        maxRightDepth = -1;
+        rightSideView(root, ans, 0);
+        return ans;
+    }
+
+    public void rightSideView(TreeNode root, List<Integer> ans, int depth) {
+
+        if (root == null) {
+            return;
+        }
+        if (maxRightDepth < depth) {
+            maxRightDepth = depth;
+            ans.add(root.val);
+        }
+        rightSideView(root.right, ans, depth + 1);
+        rightSideView(root.left, ans, depth + 1);
     }
 }
