296d3acd50a40c2653030c81e5d648f3ff704e6a
ztgreat <1170881778@qq.com>
2020-01-14 22:49:37 +0800

    二叉树的右视图

---
+++ src/main/java/com/github/ztgreat/dfs/leetcode_199/Solution.java
@@ -0,0 +1,21 @@
+package com.github.ztgreat.dfs.leetcode_199;
+
+
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+
+    public List<Integer> rightSideView(TreeNode root) {
+
+    }
+}
