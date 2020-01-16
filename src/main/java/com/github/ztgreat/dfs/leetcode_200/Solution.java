6c739180c37fab7db77d747dfb7db02472db162a
ztgreat <1170881778@qq.com>
2020-01-16 19:09:38 +0800

    岛屿数量

--- src/main/java/com/github/ztgreat/dfs/leetcode_200/Solution.java
+++ src/main/java/com/github/ztgreat/dfs/leetcode_200/Solution.java
@@ -1,8 +1,34 @@
 package com.github.ztgreat.dfs.leetcode_200;
 
-
+// 简单遍历dfs就可以了
 class Solution {
     public int numIslands(char[][] grid) {
 
+        int cnt = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+
+                if (grid[i][j] == '1') {
+                    cnt++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return cnt;
+    }
+
+    public void dfs(char[][] grid, int x, int y) {
+
+        if (x < 0 || y < 0 || x >= grid.length || y >= grid[x].length) {
+            return;
+        }
+        if (grid[x][y] == '1') {
+            grid[x][y] = '0';
+            dfs(grid, x + 1, y);
+            dfs(grid, x - 1, y);
+            dfs(grid, x, y + 1);
+            dfs(grid, x, y - 1);
+
+        }
     }
 }
