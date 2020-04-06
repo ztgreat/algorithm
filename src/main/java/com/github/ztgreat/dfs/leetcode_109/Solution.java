5207f418ce60a72aae3616fea3ae893b4bff6c2e
ztgreat <1170881778@qq.com>
2020-04-06 21:38:22 +0800

    有序链表转换二叉搜索树

--- src/main/java/com/github/ztgreat/dfs/leetcode_109/Solution.java
+++ src/main/java/com/github/ztgreat/dfs/leetcode_109/Solution.java
@@ -23,5 +23,27 @@ class TreeNode {
 class Solution {
     public TreeNode sortedListToBST(ListNode head) {
 
+        if (head == null) {
+            return null;
+        } else if (head.next == null) {
+            return new TreeNode(head.val);
+        }
+
+        // 找中间节点
+        ListNode pre = head;
+        ListNode q = pre.next.next;
+        while (q != null && q.next != null) {
+            pre = pre.next;
+            q = q.next.next;
+        }
+        ListNode p = pre.next;
+        pre.next = null;
+        TreeNode root = new TreeNode(p.val);
+        root.left = sortedListToBST(head);
+        root.right = sortedListToBST(p.next);
+        return root;
+
     }
+
+
 }
