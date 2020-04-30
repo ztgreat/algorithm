6d3d187ed564aed903de0f8cd297f0bda8eb3760
ztgreat <1170881778@qq.com>
2020-04-30 15:05:46 +0800

    删除链表的倒数第N个节点

---
+++ src/main/java/com/github/ztgreat/linkedList/leetcode_19/Solution.java
@@ -0,0 +1,45 @@
+package com.github.ztgreat.linkedList.leetcode_19;
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+/**
+ * 快慢指针，快指针先走n步，然后快慢一起走，直到快指针走到最后.
+ * 要注意的是可能是要删除第一个节点，这个时候可以直接返回head -> next
+ */
+class Solution {
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        if (head == null) {
+            return head;
+        }
+        if (head.next == null && n == 1) {
+            return null;
+        }
+        ListNode first = head;
+        ListNode second = head;
+        for (int i = 0; i < n; i++) {
+            first = first.next;
+        }
+        if (first == null) {
+            head = head.next;
+            return head;
+        }
+        while (first.next != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return head;
+
+    }
+
+}
