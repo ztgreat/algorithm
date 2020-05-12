242d788e56daef17e26fe30f0b88d87d7e2f325c
ztgreat <1170881778@qq.com>
2020-05-12 19:24:26 +0800

    跳跃游戏，dp 解法

---
+++ src/main/java/com/github/ztgreat/dp/leetcode_55/leetcode_53/Solution.java
@@ -0,0 +1,32 @@
+package com.github.ztgreat.dp.leetcode_55.leetcode_53;
+
+
+/**
+ * dp 解法
+ * dp[i] = dp[j]  && dp[j] 能到达 && nums[j] >= i - j
+ */
+class Solution {
+
+    public boolean canJump(int[] nums) {
+
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        if (nums[0] == 0 && nums.length == 1) {
+            return true;
+        }
+        boolean[] dp = new boolean[nums.length];
+        dp[0] = true;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (dp[j] && nums[j] >= i - j) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[nums.length - 1];
+
+    }
+}
