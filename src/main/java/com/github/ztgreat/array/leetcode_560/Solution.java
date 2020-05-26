732f1595910772e91a8776026a24d561342ab4a7
ztgreat <1170881778@qq.com>
2020-05-26 22:36:00 +0800

    暴力版，和为K的子数组

--- src/main/java/com/github/ztgreat/array/leetcode_560/Solution.java
+++ src/main/java/com/github/ztgreat/array/leetcode_560/Solution.java
@@ -5,6 +5,26 @@ class Solution {
 
     public int subarraySum(int[] nums, int k) {
 
+        if (nums == null) {
+            return 0;
+        }
+        int[] dp = new int[nums.length + 1];
+        dp[1] = nums[0];
+        int cnt = 0;
+        if (dp[1] == k) {
+            cnt++;
+        }
+        for (int i = 1; i < nums.length; i++) {
+            dp[i + 1] = dp[i] + nums[i];
+
+            for (int j = 0; j <= i; j++) {
+                if (dp[i + 1] - dp[j] == k) {
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+
     }
 
 
